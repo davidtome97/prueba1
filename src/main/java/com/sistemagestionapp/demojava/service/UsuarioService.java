dd2137d33d67ae1920e927a79406fe3f6b56df18
davidtome97 <25766791+davidtome97@users.noreply.github.com>
2025-12-19 16:21:54 +0100

    fix: remove duplicate services + fix UsuarioService imports

--- src/main/java/com/sistemagestionapp/demojava/service/UsuarioService.java
+++ src/main/java/com/sistemagestionapp/demojava/service/UsuarioService.java
@@ -1,3 +1,22 @@
+package com.sistemagestionapp.demojava.service;
+
+import com.sistemagestionapp.demojava.model.Usuario;
+import com.sistemagestionapp.demojava.model.mongo.UsuarioMongo;
+import com.sistemagestionapp.demojava.repository.UsuarioRepository;
+import com.sistemagestionapp.demojava.repository.mongo.UsuarioMongoRepository;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 @Service
 public class UsuarioService implements UserDetailsService {
 
